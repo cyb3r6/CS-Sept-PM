4f1350b513c1f9564db0a5ab514ab64280b800ce
cyb3r6 <45832126+cyb3r6@users.noreply.github.com>
2019-11-25 21:13:37 -0700

    Session 17

---
+++ Sept PM/Assets/Scripts/SpringButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringButton : MonoBehaviour
+{
+    public Light light;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "SpringButton")
+        {
+            light.enabled = !light.enabled;
+            // false = true
+            // true = false
+        }
+    }
+}
